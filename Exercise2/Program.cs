094fa540169101abe41aaaadb0f27d9f252cfda7
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-10-31 18:03:04 +0800

    create public void bubble short

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -1,40 +1,55 @@
 ﻿using System;
 
-class proram
-{
-    //mendeklarasikan array int dengan ukuran 66
-    private int[] rennu = new int[66];
-    private int n;
 
-    public void inputData()
+namespace Exercise2
+{
+    class proram
     {
-        while (true)
+        //mendeklarasikan array int dengan ukuran 66
+        private int[] rennu = new int[66];
+        private int n;
+
+        public void inputData()
         {
-            Console.Write("masukkan jumlah data pada array: ");
-            string s = Console.ReadLine();
-            n = int.Parse(s);
-            if (n < 66)
-                break;
-            else
-                Console.WriteLine("\nMaksimal jumlah data pada array adalah 66. \n");
+            while (true)
+            {
+                Console.Write("masukkan jumlah data pada array: ");
+                string s = Console.ReadLine();
+                n = int.Parse(s);
+                if (n < 66)
+                    break;
+                else
+                    Console.WriteLine("\nMaksimal jumlah data pada array adalah 66. \n");
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("$masukkan elemen array: ");
+                string s2 = Console.ReadLine();
+                rennu[i] = int.Parse(s2);
+            }
         }
-        for (int i = 0; i < n; i++)
+        public void displayData()
         {
-            Console.WriteLine("$masukkan elemen array: ");
-            string s2 = Console.ReadLine();
-            rennu[i] = int.Parse(s2);
+            Console.WriteLine("");
+            Console.WriteLine("===================================");
+            Console.WriteLine("Elemen Array yang telah terususun: ");
+            Console.WriteLine("===================================");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{i}. {rennu[i]}");
+            }
+            Console.WriteLine();
+        }
+        public void bubbleshort()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int sls = 0; sls < n; sls++);
+                {
+                    if(rennu)
+                }
+            }
         }
     }
-    public void displayData()
-    {
-        Console.WriteLine("");
-        Console.WriteLine("===================================");
-        Console.WriteLine("Elemen Array yang telah terususun: ");
-        Console.WriteLine("===================================");
-        for (int i = 0; i < n; i++)
-        {
-            Console.WriteLine($"{i}. {rennu[i]}");
-        }
-        Console.WriteLine();
-    }
+
 }
