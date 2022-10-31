51fd8008b11ad8e05f5bc7086468641e8fd35707
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-10-31 17:31:51 +0800

    make while to determine the amount of data received

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -2,9 +2,22 @@
 
 class proram
 {
-    
+    //mendeklarasikan array int dengan ukuran 66
     private int[] rennu = new int[66];
     private int n;
 
-    
+    public void inputData()
+    {
+        while (true)
+        {
+            Console.Write("masukkan jumlah data pada array: ");
+            string s = Console.ReadLine();
+            n = int.Parse(s);
+            if (n < 66)
+                break;
+            else
+                Console.WriteLine("\nMaksimal jumlah data pada array : ");
+        }
+        
+    }
 }
